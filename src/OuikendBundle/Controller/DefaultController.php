e9d062abb2aad5eaf5cd96828f171afd27601183
martinezmanuel <martinez-manuel@neuf.fr>
2016-06-17 02:15:08 +0200

    miracle

--- src/OuikendBundle/Controller/DefaultController.php
+++ src/OuikendBundle/Controller/DefaultController.php
@@ -2,12 +2,17 @@
 
 namespace OuikendBundle\Controller;
 
+use OuikendBundle\Entity\Trips;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        // replace this example code with whatever you need
         return $this->render('OuikendBundle:Default:index.html.twig');
     }
+
+   
 }
